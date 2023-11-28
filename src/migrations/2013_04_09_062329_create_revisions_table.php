fa93891782f9084dd1aea926c88617fd00cb6b67
Giovanni Ledda <ledda@netseven.it>
2023-11-28 10:47:08 +0100

    Update 2013_04_09_062329_create_revisions_table.php
    old_value and new_value from text to json

--- src/migrations/2013_04_09_062329_create_revisions_table.php
+++ src/migrations/2013_04_09_062329_create_revisions_table.php
@@ -17,8 +17,8 @@ class CreateRevisionsTable extends Migration
             $table->unsignedBigInteger('revisionable_id');
             $table->unsignedBigInteger('user_id')->nullable();
             $table->string('key');
-            $table->text('old_value')->nullable();
-            $table->text('new_value')->nullable();
+            $table->json('old_value')->nullable();
+            $table->json('new_value')->nullable();
             $table->timestamps();
 
             $table->index(array('revisionable_id', 'revisionable_type'));
